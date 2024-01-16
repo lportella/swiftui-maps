f3b0195a840823cd5765d2d21f1f0b28c1059282
Lucas Portella <lucasportella@Lucass-MacBook-Pro.local>
2024-01-15 22:29:10 -0300

    added picker to switch through map styles

--- MapsMaps/ContentView.swift
+++ MapsMaps/ContentView.swift
@@ -9,9 +9,19 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State private var selectedMapOption: MapOptions = .standard
+    
     var body: some View {
         ZStack(alignment: .top) {
             Map()
+                .mapStyle(selectedMapOption.mapStyle)
+            Picker("Map Styles", selection: $selectedMapOption) {
+                ForEach(MapOptions.allCases) { option in
+                    Text(option.rawValue.capitalized).tag(option)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding()
         }
     }
 }
