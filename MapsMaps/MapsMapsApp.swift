d3d9c9301751fbe83b1644b3f82d29c94d813805
Lucas Portella <lucasportella@Lucass-MacBook-Pro.local>
2024-01-15 21:52:48 -0300

    first commit

---
+++ MapsMaps/MapsMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsMapsApp.swift
+//  MapsMaps
+//
+//  Created by Lucas Portella on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MapsMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
